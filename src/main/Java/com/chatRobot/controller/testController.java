ec614d92f81aa6e92fdb4cb88e8a2f748a7f5212
fawks96 <443118619@qq.com>
2018-03-30 00:02:33 +0800

    add get questions API

--- src/main/Java/com/chatRobot/controller/testController.java
+++ src/main/Java/com/chatRobot/controller/testController.java
@@ -14,21 +14,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
-import net.sf.json.JSONArray;
 import com.chatRobot.service.TestService;
-import com.chatRobot.model.getQueData;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.chatRobot.model.questionsRes;
 @Controller
 @RequestMapping("/test")
 public class testController {
 
     @Resource
-   // @Autowired
+    // @Autowired
     private TestService testService;
 
-    @RequestMapping(value = "/questions", method = RequestMethod.GET)
-    public @ResponseBody List<QuestionInfo> getQuestions(HttpServletRequest request)
-    {
+    @RequestMapping(value = "/questions2", method = RequestMethod.GET)
+    public @ResponseBody
+    List<QuestionInfo> getQuestions2(HttpServletRequest request) {
         String category = request.getParameter("category");
         int num = Integer.parseInt(request.getParameter("num"));
 
@@ -36,15 +34,11 @@ public class testController {
 
         int len = questionList.size();
 
-        if (len <= num)
-        {
+        if (len <= num) {
             return questionList;
-        }
-        else
-        {
+        } else {
             List<QuestionInfo> newQuestionList = new ArrayList<>();
-            for (int i = 0; i < num; i++)
-            {
+            for (int i = 0; i < num; i++) {
                 newQuestionList.add(questionList.get(i));
             }
             return newQuestionList;
@@ -53,32 +47,30 @@ public class testController {
 
     }
 
-   /*@RequestMapping(value = "/questions2", method = RequestMethod.POST)
-    public @ResponseBody void getQuestions2(@RequestBody Map<String,String> map)
-    {
+    @RequestMapping(value = "/questions", method = RequestMethod.POST)
+    public @ResponseBody
+    questionsRes getQuestions(@RequestBody Map<String, Object> map) {
         System.out.println("fuck");
-        int num = Integer.parseInt(map.get("num"));
-        System.out.println(num);
 
-        JSONObject myJson = JSONObject.fromObject(map.get("CategoryList"));
-        //JSONArray list = new JSONArray("233");
-        JSONArray list = myJson.getJSONArray();
-        for (int i = 0; i < list.length(); i++)
+        questionsRes myRes = new questionsRes();
+
+        int num = Integer.parseInt(map.get("num").toString());
+
+        System.out.println(num);
+        ArrayList CategoryList = (ArrayList) map.get("CategoryList");
+        System.out.println(CategoryList.size());
+        for (int i = 0; i < CategoryList.size(); i++)
         {
-            System.out.println(list.getString(i));
+            System.out.println(CategoryList.get(i));
         }
-    //    System.out.println(quedata.getNum());
-      //  System.out.println(quedata.getCategoryList());
-   /*     List<String> CategoryList = quedata.getCategoryList();
-        int num = quedata.getNum();
-        for (int i = 0; i < CategoryList.size(); i++) System.out.println(CategoryList.get(i));
-        System.out.println("num:"+num);
+
+
 
         List<QuestionInfo> questionList = new ArrayList<>();
 
         for (int i = 0; i < CategoryList.size(); i++)
         {
-            List<QuestionInfo> tmpList = testService.getByCategory(CategoryList.get(i));
+            List<QuestionInfo> tmpList = testService.getByCategory(CategoryList.get(i).toString());
             for (int j = 0; j < tmpList.size(); j++)
             {
                 questionList.add(tmpList.get(j));
@@ -90,18 +82,29 @@ public class testController {
 
         if (num >= len)
         {
-            return questionList;
+            if (questionList == null) myRes.setStatus("Fail");
+            else if (questionList.size() == 0) myRes.setStatus("题库暂空");
+            else myRes.setStatus("Ok");
+
+            myRes.setQuestionList(questionList);
+            return myRes;
         }
         else
         {
             List<QuestionInfo> resList = new ArrayList<>();
             for (int i = 0; i < num; i++) resList.add(questionList.get(i));
-            return resList;
-        }
-      //  testService.getByCategory(category);
 
+            if (resList == null) myRes.setStatus("Fail");
+            else if (resList.size() == 0) myRes.setStatus("题库暂空");
+            else myRes.setStatus("Ok");
+
+            myRes.setQuestionList(resList);
+            return myRes;
         }
-            */
+
     }
+
+
+}
 
 
